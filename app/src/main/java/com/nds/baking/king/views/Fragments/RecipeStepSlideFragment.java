47564a6ea521f0ecce371f41da9503f36bc6c36b
Namrata Shah <namrata.shah@verizonwireless.com>
2017-05-22 23:10:04 -0400

    Adding assets for navigation icons

---
+++ app/src/main/java/com/nds/baking/king/views/Fragments/RecipeStepSlideFragment.java
@@ -0,0 +1,8 @@
+package com.nds.baking.king.views.Fragments;
+
+/**
+ * Created by c0shan7 on 5/21/2017.
+ */
+
+public class RecipeStepSlideFragment {
+}
---
+++ app/src/main/java/com/nds/baking/king/views/adapter/RecipeDetailPagerAdapter.java
@@ -0,0 +1,8 @@
+package com.nds.baking.king.views.adapter;
+
+/**
+ * Created by c0shan7 on 5/21/2017.
+ */
+
+public class RecipeDetailPagerAdapter {
+}
