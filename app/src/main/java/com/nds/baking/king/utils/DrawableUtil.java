db4cbe1ff1d154a079e5509554ce1f271a4af5b9
Namrata Shah <namrata.shah8092@gmail.com>
2017-07-25 22:02:35 -0400

    updated util file

--- app/src/main/java/com/nds/baking/king/utils/DrawableUtil.java
+++ app/src/main/java/com/nds/baking/king/utils/DrawableUtil.java
@@ -6,13 +6,14 @@ import android.os.Build;
 import android.widget.ImageView;
 
 /**
- * Created by c0shan7 on 7/22/2017.
+ * Created by Namrata Shah on 7/22/2017.
  */
 
 public final class DrawableUtil {
 
     private DrawableUtil(){};
 
+	/*  check for deprecated API before retriving drawable*/
     public static Drawable getDrawableImage(Context mContext,int drawableID){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             return mContext.getResources().getDrawable(drawableID ,null);
@@ -20,7 +21,7 @@ public final class DrawableUtil {
             return mContext.getResources().getDrawable(drawableID);
         }
     }
-
+/*  check for deprecated API before setting drawable image*/
     public static void setDrawableToImageView(Drawable drawable, ImageView imageView){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             imageView.setBackground(drawable);
